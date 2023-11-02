9607feca5c5be6ca7e09cb591247fe3d52935e97
surfacez <sergiobatistajr94@gmail.com>
2023-11-02 20:01:50 -0300

    add toggle button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,7 @@
+import { ModeToggle } from "@/components/ModeToggle"
 import { UserAuthForm } from "@/components/UserAuthForm"
+import { Button, buttonVariants } from "@/components/ui/button"
+
 import Image from "next/image"
 
 export default function Page() {
@@ -21,14 +24,13 @@ export default function Page() {
         />
       </div>
       <div className="container relative hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        {/* <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
+        <Button
+          type="button"
+          className="absolute right-4 top-4 md:right-8 md:top-8"
+          variant="ghost"
         >
-        </Link> */}
+          <ModeToggle />
+        </Button>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
