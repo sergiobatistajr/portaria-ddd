64c1838ffe17657b77dc0e0e62cbc4b21ac8d99b
surfacez <sergiobatistajr94@gmail.com>
2023-11-07 13:54:44 -0300

    adding search

--- src/app/dashboard/exit/page.tsx
+++ src/app/dashboard/exit/page.tsx
@@ -3,6 +3,7 @@ import { fetchCountGuestsPages } from "@/lib/data"
 import Pagination from "@/components/Pagination"
 import { Suspense } from "react"
 import ExitDateTable from "./ExitDataTable"
+import Search from "@/components/Search"
 
 export default async function Page({
   searchParams,
@@ -19,7 +20,8 @@ export default async function Page({
   return (
     <main>
       <h1 className="flex justify-center text-3xl">Saídas</h1>
-      <div className="mt-2">
+      <div className="mt-2 space-y-2">
+        <Search placeholder="Procurar visitante..." />
         <Suspense key={query + currentPage} fallback={<h1>Carregando</h1>}>
           <ExitDateTable query={query} currentPage={currentPage} />
         </Suspense>
