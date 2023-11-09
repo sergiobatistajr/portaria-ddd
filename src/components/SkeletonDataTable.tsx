706362d674ce104e5d0594460650c4a110f698fb
surfacez <sergiobatistajr94@gmail.com>
2023-11-09 15:53:26 -0300

    better sekeleton

--- src/components/SkeletonDataTable.tsx
+++ src/components/SkeletonDataTable.tsx
@@ -3,18 +3,18 @@ import { Skeleton } from "./ui/skeleton"
 export default function SkeletonDataTable() {
   return (
     <div className="flex flex-col items-center space-y-0.5">
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
-      <Skeleton className="h-[46.4px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
+      <Skeleton className="h-[56px] w-full" />
     </div>
   )
 }
