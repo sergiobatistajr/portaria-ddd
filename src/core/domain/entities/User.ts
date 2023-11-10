4b8f754a0d0de60b2fffffecec5c46af51af8dae
surfacez <sergiobatistajr94@gmail.com>
2023-11-10 13:07:49 -0300

    Add role and status properties to User entity

--- src/core/domain/entities/User.ts
+++ src/core/domain/entities/User.ts
@@ -1,20 +1,33 @@
 import Email from "../shared/Email"
 import Id from "../shared/Id"
 import NameLastName from "../shared/NameLastName"
+import UserRole from "../shared/UserRole"
+import UserStatus from "../shared/UserStatus"
 
 export default class User {
   private constructor(
     readonly id: string,
     readonly name: string,
     readonly email: string,
+    readonly role: string,
+    readonly status: string,
     readonly password: string
   ) {}
 
-  static create(name: string, email: string, password: string, id?: string) {
+  static create(
+    name: string,
+    email: string,
+    password: string,
+    role: string,
+    status: string,
+    id?: string
+  ) {
     const _id = id ? id : Id.generate()
     const _name = NameLastName.validade(name)
     const _email = Email.validade(email)
     const _password = password
-    return new User(_id, _name, _email, _password)
+    const _role = UserRole.validate(role)
+    const _status = UserStatus.validade(status)
+    return new User(_id, _name, _email, _role, _status, _password)
   }
 }
