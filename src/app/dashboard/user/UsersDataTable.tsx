09583d3af5f15fd0d58c1dd135bb94fbffb8115b
surfacez <sergiobatistajr94@gmail.com>
2023-11-10 13:07:15 -0300

    Update UsersDataTable to display user status and role

--- src/app/dashboard/user/UsersDataTable.tsx
+++ src/app/dashboard/user/UsersDataTable.tsx
@@ -14,7 +14,8 @@ export default async function UsersDataTable({
     return {
       nomeCompleto: u.name,
       email: u.email,
-      ativo: "Ativo",
+      ativo: u.status,
+      funcao: u.role,
     }
   })
 
