90fcc1889e7008ce0de446cea842d6f1abe1e3c0
surfacez <sergiobatistajr94@gmail.com>
2023-11-10 13:07:40 -0300

    Refactor User.create tests to include more edge cases

--- src/core/domain/entities/User.test.ts
+++ src/core/domain/entities/User.test.ts
@@ -1,36 +1,57 @@
 import User from "./User"
 
 describe("User", () => {
-  it("should create a user with valid data", () => {
-    const user = User.create("John Doe", "john.doe@example.com", "password")
-    expect(user).toBeInstanceOf(User)
-    expect(user.name).toBe("John Doe")
-    expect(user.email).toBe("john.doe@example.com")
-    expect(user.password).toBe("password")
-  })
-  it("should create a user with a custom ID", () => {
-    const user = User.create(
-      "John Doe",
-      "john.doe@example.com",
-      "password",
-      "custom-id"
-    )
-    expect(user).toBeInstanceOf(User)
-    expect(user.id).toBe("custom-id")
-    expect(user.name).toBe("John Doe")
-    expect(user.email).toBe("john.doe@example.com")
-    expect(user.password).toBe("password")
-  })
+  describe("create", () => {
+    it("should create a user with valid parameters", () => {
+      const user = User.create(
+        "John Doe",
+        "johndoe@example.com",
+        "password",
+        "admin",
+        "active"
+      )
+      expect(user).toBeInstanceOf(User)
+      expect(user.name).toBe("John Doe")
+      expect(user.email).toBe("johndoe@example.com")
+      expect(user.password).toBe("password")
+      expect(user.role).toBe("admin")
+      expect(user.status).toBe("active")
+    })
 
-  it("should throw an error when creating a user with invalid data", () => {
-    expect(() => {
-      User.create("", "john.doe@example.com", "password")
-    }).toThrow("Nome é obrigatório")
-    expect(() => {
-      User.create("jhon", "john.doe@example.com", "password")
-    }).toThrow("Sobrenome é obrigatório")
-    expect(() => {
-      User.create("John Doe", "invalid-email", "password")
-    }).toThrow("Email inválido")
+    it("should throw an error for an invalid email", () => {
+      expect(() =>
+        User.create("John Doe", "invalidemail", "password", "admin", "active")
+      ).toThrow("Email inválido")
+    })
+
+    it("should throw an error for an invalid name", () => {
+      expect(() =>
+        User.create("", "johndoe@example.com", "password", "admin", "active")
+      ).toThrow("Nome é obrigatório")
+    })
+
+    it("should throw an error for an invalid role", () => {
+      expect(() =>
+        User.create(
+          "John Doe",
+          "johndoe@example.com",
+          "password",
+          "invalidrole",
+          "active"
+        )
+      ).toThrow("Função inválida")
+    })
+
+    it("should throw an error for an invalid status", () => {
+      expect(() =>
+        User.create(
+          "John Doe",
+          "johndoe@example.com",
+          "password",
+          "admin",
+          "invalidstatus"
+        )
+      ).toThrow("Status inválido")
+    })
   })
 })
