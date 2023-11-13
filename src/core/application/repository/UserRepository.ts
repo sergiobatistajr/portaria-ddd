d6e982f4332503675bc2f82d9325ff043db5e9af
surfacez <sergiobatistajr94@gmail.com>
2023-11-13 13:42:42 -0300

    Add findById and update methods to UserRepository

--- src/core/application/repository/UserRepository.ts
+++ src/core/application/repository/UserRepository.ts
@@ -1,7 +1,9 @@
 import User from "@/core/domain/entities/User"
 
 export default interface UserRepository {
+  findById(id: string): Promise<User | null>
   findByEmail(email: string): Promise<User | null>
+  update(user: Omit<User, "password">): Promise<void>
   save(user: User): Promise<void>
   delete(id: string): Promise<void>
   findFilteredUsers(
