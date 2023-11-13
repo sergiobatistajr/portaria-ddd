a70c72e354e54694ea1d14f363e5df0bd04440ff
surfacez <sergiobatistajr94@gmail.com>
2023-11-13 17:19:56 -0300

    Add password reset page with form

---
+++ src/app/dashboard/user/password-reset/[id]/page.tsx
@@ -0,0 +1,24 @@
+import ResetPasswordForm from "./Form"
+
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: {
+    id: string
+  }
+  searchParams?: {
+    query?: string
+  }
+}) {
+  const name = searchParams?.query || "Inválido"
+  return (
+    <main className="space-y-1">
+      <h1 className="text-3xl">Resetar Senha</h1>
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <p>
+        <ResetPasswordForm id={params.id} />
+      </p>
+    </main>
+  )
+}
