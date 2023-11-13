4e7f4265759ae3d5e7d0102ab9be9f597f0589ef
surfacez <sergiobatistajr94@gmail.com>
2023-11-13 17:19:52 -0300

    Add password reset form component

---
+++ src/app/dashboard/user/password-reset/[id]/Form.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { resetPassword } from "@/lib/actions"
+import { useFormState } from "react-dom"
+
+export default function ResetPasswordForm({ id }: { id: string }) {
+  const [state, action] = useFormState(resetPassword, undefined)
+
+  return (
+    <form action={action} className="space-y-4">
+      <input name="id" type="text" defaultValue={id} className="sr-only" />
+      <Label htmlFor="password">Senha</Label>
+      <Input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="**********"
+        required
+      />
+      <Label htmlFor="confirmPassword">Cofirme a senha</Label>
+      <Input
+        id="confirmPassword"
+        name="confirmPassword"
+        type="password"
+        placeholder="**********"
+        required
+      />
+      {state?.message && (
+        <div>
+          <span className="text-rose-700 text-xl">{state?.message}</span>
+        </div>
+      )}
+      <Button type="submit">Salvar</Button>
+    </form>
+  )
+}
