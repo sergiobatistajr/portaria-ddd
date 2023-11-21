7d525f632c67fd973757f40520ed7539892fd533
surfacez <sergiobatistajr94@gmail.com>
2023-11-21 11:38:07 -0300

    Refactor userController to use a single usecase object

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,25 +1,36 @@
 import { Request, Response } from "express"
 import FindFilteredUsers from "../core/application/usecase/FindFilteredUsers"
 import CountUsersPage from "../core/application/usecase/CountUsersPage"
-
+interface Usecases {
+  findFilteredUsers: FindFilteredUsers
+  countUsersPage: CountUsersPage
+}
 export default class UserController {
-  constructor(
-    private readonly findFilteredUsersUsecase: FindFilteredUsers,
-    private readonly countUsersPageUsecase: CountUsersPage
-  ) {}
+  constructor(private readonly usecase: Usecases) {}
 
-  async findFilteredUsers(req: Request, res: Response): Promise<Response> {
+  async findFilteredUsers(input: Input): Promise<Output> {
+    const { req, res } = input
     const { query = "", currentPage = 1 } = req.query
-    const users = await this.findFilteredUsersUsecase.execute(
+    const users = await this.usecase.findFilteredUsers.execute(
       query as string,
       Number(currentPage)
     )
 
     return res.status(200).json(users)
   }
-  async countUsersPage(req: Request, res: Response): Promise<Response> {
+  async countUsersPage(input: Input): Promise<Output> {
+    const { req, res } = input
     const { query = "" } = req.query
-    const totalPages = await this.countUsersPageUsecase.execute(query as string)
+    const totalPages = await this.usecase.countUsersPage.execute(
+      query as string
+    )
     return res.status(200).json(totalPages)
   }
 }
+
+type Input = {
+  req: Request
+  res: Response
+}
+
+type Output = Response
