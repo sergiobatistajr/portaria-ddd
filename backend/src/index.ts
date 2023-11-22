cbcad84783fdd878dea914c3a1ef955449f767c0
surfacez <sergiobatistajr94@gmail.com>
2023-11-22 13:50:37 -0300

    Add user-related controllers and use cases

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,14 @@ import LoginController from "./controllers/LoginController"
 import RegisterUser from "./core/application/usecase/RegisterUser"
 import RegisterUserController from "./controllers/RegisterController"
 import AuthMiddleware from "./middleware"
+import UpdateUser from "./core/application/usecase/UpdateUser"
+import UpdateUserController from "./controllers/UpdateUserController"
+import FindFilteredUsers from "./core/application/usecase/FindFilteredUsers"
+import FindFilteredUserController from "./controllers/FindFilteredUserController"
+import FindUserById from "./core/application/usecase/FindUserById"
+import FindUserByIdController from "./controllers/FindUserByIdController"
+import CountUsersPage from "./core/application/usecase/CountUsersPage"
+import CountUsersPageController from "./controllers/CountUsersPageController"
 
 let dbInstance: IDatabase<any>
 export function getDbInstance() {
@@ -29,6 +37,18 @@ const authMiddleware = new AuthMiddleware()
 //protegidas
 const registerUser = RegisterUser.getInstance(userDb)
 new RegisterUserController(app, registerUser, authMiddleware)
+
+const updateUser = UpdateUser.getInstance(userDb)
+new UpdateUserController(app, updateUser, authMiddleware)
+
+const findFilteredUsers = FindFilteredUsers.getInstance(userDb)
+new FindFilteredUserController(app, findFilteredUsers, authMiddleware)
+
+const countUsersPage = CountUsersPage.getInstance(userDb)
+new CountUsersPageController(app, countUsersPage, authMiddleware)
+
+const findUserById = FindUserById.getInstance(userDb)
+new FindUserByIdController(app, findUserById, authMiddleware)
 
 app.listen(3001, () => {
   console.log("Aplicação está rodando na porta 3001")
