93c3fc8628090a87d8c248c8d3ed997679371f74
surfacez <sergiobatistajr94@gmail.com>
2023-11-22 17:59:14 -0300

    Update search debounce timer

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -18,7 +18,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete("query")
     }
     replace(`${pathname}?${params.toString()}`)
-  }, 500)
+  }, 200)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
