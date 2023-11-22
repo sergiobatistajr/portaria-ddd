af5a4fe08885d4e0a608a0a3d05b57f5908a788f
surfacez <sergiobatistajr94@gmail.com>
2023-11-22 10:48:06 -0300

    Refactor backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,11 @@
-import express, { Request, Response } from "express"
+import express from "express"
 import pgPromise, { IDatabase } from "pg-promise"
 import UserRepositoryDatabase from "./core/infra/db/UserRepositoryDatabase"
-import FindFilteredUsers from "./core/application/usecase/FindFilteredUsers"
-import UserController from "./controllers/UserController"
-import CountUsersPage from "./core/application/usecase/CountUsersPage"
-import RegisterUser from "./core/application/usecase/RegisterUser"
 import LoginUser from "./core/application/usecase/LoginUser"
-import AuthController from "./controllers/AuthController"
-import authMiddleware from "./middleware"
-import UpdateUser from "./core/application/usecase/UpdateUser"
-import FindUserById from "./core/application/usecase/FindUserById"
+import LoginController from "./controllers/LoginController"
+import RegisterUser from "./core/application/usecase/RegisterUser"
+import RegisterUserController from "./controllers/RegisterController"
+import AuthMiddleware from "./middleware"
 
 let dbInstance: IDatabase<any>
 export function getDbInstance() {
@@ -21,63 +17,19 @@ export function getDbInstance() {
 }
 //dbs
 const userDb = UserRepositoryDatabase.getInstance(getDbInstance())
-//casos de uso
-const findFilteredUsers = FindFilteredUsers.getInstance(userDb)
-const countUsersPage = CountUsersPage.getInstance(userDb)
-const registerUser = RegisterUser.getInstance(userDb)
-const loginUser = LoginUser.getInstance(userDb)
-const updateUser = UpdateUser.getInstance(userDb)
-const findUserById = FindUserById.getInstance(userDb)
-//controllers
-const userController = new UserController({
-  findFilteredUsers,
-  countUsersPage,
-  updateUser,
-  findUserById,
-})
-const authController = new AuthController({ loginUser, registerUser })
+
 //express
 const app = express()
 app.use(express.json())
 
-//rotas
-app.post(
-  "/auth/login",
-  async (req: Request, res: Response) =>
-    await authController.login({ req, res })
-)
-app.post(
-  "/auth/register",
-  async (req: Request, res: Response) =>
-    await authController.resgiter({ req, res })
-)
-//teste protegida
-app.get(
-  "/users",
-  authMiddleware,
-  async (req: Request, res: Response) =>
-    await userController.findFilteredUsers({ req, res })
-)
-app.get(
-  "/users/count",
-  authMiddleware,
-  async (req: Request, res: Response) =>
-    await userController.countUsersPage({ req, res })
-)
-app.get(
-  "/users/:id",
-  authMiddleware,
-  async (req: Request, res: Response) =>
-    await userController.findUserById({ req, res })
-)
+const loginUser = LoginUser.getInstance(userDb)
+new LoginController(app, loginUser)
+//midleware
+const authMiddleware = new AuthMiddleware()
+//protegidas
+const registerUser = RegisterUser.getInstance(userDb)
+new RegisterUserController(app, registerUser, authMiddleware)
 
-app.patch(
-  "/users/update/:id",
-  authMiddleware,
-  async (req: Request, res: Response) =>
-    await userController.updateUser({ req, res })
-)
-//server listen
 app.listen(3001, () => {
   console.log("Aplicação está rodando na porta 3001")
 })
