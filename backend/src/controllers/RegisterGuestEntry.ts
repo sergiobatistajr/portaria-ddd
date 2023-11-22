81068508745ba92d669a0209f33193bf4f1d4015
surfacez <sergiobatistajr94@gmail.com>
2023-11-22 19:54:03 -0300

    Add RegisterGuestEntryController to handle guest registration

---
+++ backend/src/controllers/RegisterGuestEntry.ts
@@ -0,0 +1,39 @@
+import { Express } from "express"
+import RegisterGuestEntry from "../core/application/usecase/RegisterGuestEntry"
+
+export default class RegisterGuestEntryController {
+  constructor(
+    readonly server: Express,
+    readonly useCase: RegisterGuestEntry,
+    ...middleware: any[]
+  ) {
+    server.post("/guests", ...middleware, async (req, res) => {
+      try {
+        const {
+          name,
+          entryDate,
+          createdBy,
+          plate,
+          model,
+          pax,
+          apartment,
+          observation,
+        } = req.body
+        await useCase.execute({
+          createdBy,
+          entryDate,
+          name,
+          apartment,
+          model,
+          observation,
+          pax,
+          plate,
+        })
+      } catch (error) {
+        if (error instanceof Error) {
+          return res.status(500).send(error.message).end()
+        }
+      }
+    })
+  }
+}
