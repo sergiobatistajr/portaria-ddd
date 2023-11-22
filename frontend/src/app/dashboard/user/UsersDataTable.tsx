8174fef79ea91b4428a1c46a58ac969f387612dd
surfacez <sergiobatistajr94@gmail.com>
2023-11-22 17:59:28 -0300

    Refactor UsersDataTable mapping function

--- frontend/src/app/dashboard/user/UsersDataTable.tsx
+++ frontend/src/app/dashboard/user/UsersDataTable.tsx
@@ -13,24 +13,31 @@ export default async function UsersDataTable({
   const session = await auth()
   let columns = userColumns
   if (session?.user.role === "admin") columns = adminColumns
-
   const data = await fetchUsersFiltered(query, currentPage)
-  const users = data?.map((u) => {
-    let status: "Ativo" | "Desativado" = "Desativado"
-    let role: "Administrador" | "Porteiro" | "Relatório" = "Relatório"
-    if (u.status === "active") status = "Ativo"
-    if (u.status === "deactive") status = "Desativado"
-    if (u.role === "admin") role = "Administrador"
-    if (u.role === "user") role = "Porteiro"
-    if (u.role === "report") role = "Relatório"
-    return {
-      id: u.id,
-      nomeCompleto: u.name,
-      ativo: status,
-      email: u.email,
-      funcao: role,
+  const users = data?.map(
+    (u: {
+      status: string
+      role: string
+      id: string
+      name: string
+      email: string
+    }) => {
+      let status: "Ativo" | "Desativado" = "Desativado"
+      let role: "Administrador" | "Porteiro" | "Relatório" = "Relatório"
+      if (u.status === "active") status = "Ativo"
+      if (u.status === "deactive") status = "Desativado"
+      if (u.role === "admin") role = "Administrador"
+      if (u.role === "user") role = "Porteiro"
+      if (u.role === "report") role = "Relatório"
+      return {
+        id: u.id,
+        nomeCompleto: u.name,
+        ativo: status,
+        email: u.email,
+        funcao: role,
+      }
     }
-  })
+  )
 
   return <DataTable columns={columns} data={users ?? []} />
 }
