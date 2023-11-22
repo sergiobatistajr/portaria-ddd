8e5670e5815a90653de2c85bc7f0e6a6b39fd749
surfacez <sergiobatistajr94@gmail.com>
2023-11-22 18:49:53 -0300

    Refactor API calls and add authorization headers

--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -1,18 +1,26 @@
 "use server"
 import { unstable_noStore as noStore } from "next/cache"
-const api_url = `${process.env.EXPRESS_URL!}`
-// export async function fetchUserById(id: string) {
-//   noStore()
-//   try {
-//     const URL = `${api_url}/users/${id}`
-//     const user = await fetch(URL)
-//     return user.json()
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       return console.log(error.message)
-//     }
-//   }
-// }
+import { cookies } from "next/headers"
+const URL = `${process.env.EXPRESS_URL}`
+export async function fetchUserById(id: string) {
+  noStore()
+  try {
+    const token = cookies().get("token")?.value
+    let url = `${URL}/users/${id}`
+    const user = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return user.json()
+  } catch (error) {
+    if (error instanceof Error) {
+      return console.log(error.message)
+    }
+  }
+}
 
 // export async function fetchAllGuestFiltered(
 //   query: string,
@@ -20,7 +28,7 @@ const api_url = `${process.env.EXPRESS_URL!}`
 // ) {
 //   noStore()
 //   try {
-//     const URL = `${api_url}/guests?search=${query}&page=${currentPage}`
+//     const URL = `${URL}/guests?search=${query}&page=${currentPage}`
 //     const guests = await fetch(URL)
 //     return guests.json()
 //   } catch (error) {
@@ -71,8 +79,15 @@ const api_url = `${process.env.EXPRESS_URL!}`
 export async function fetchUsersFiltered(query: string, currentPage: number) {
   noStore()
   try {
-    let URL = `${api_url}/users?query=${query}&currentPage=${currentPage}`
-    const users = await fetch(URL)
+    const token = cookies().get("token")?.value
+    let url = `${URL}/users?query=${query}&currentPage=${currentPage}`
+    const users = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
     return users.json()
   } catch (error) {
     if (error instanceof Error) {
@@ -84,8 +99,15 @@ export async function fetchUsersFiltered(query: string, currentPage: number) {
 export async function fetchUsersPage(query: string) {
   noStore()
   try {
-    let URL = `${api_url}/users/count?query=${query}`
-    const totalPages = await fetch(URL)
+    const token = cookies().get("token")?.value
+    let url = `${URL}/users/count?query=${query}`
+    const totalPages = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
     return totalPages.json()
   } catch (error) {
     if (error instanceof Error) {
