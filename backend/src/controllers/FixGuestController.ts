8a11dd3d77d4604fc84c0d26bbcc4f4d124676e9
surfacez <sergiobatistajr94@gmail.com>
2023-11-23 17:11:20 -0300

    Add FixGuestController for handling PATCH requests

---
+++ backend/src/controllers/FixGuestController.ts
@@ -0,0 +1,45 @@
+import { Express } from "express"
+import FixGuest from "../core/application/usecase/FixGuest"
+
+export default class FixGuestController {
+  constructor(
+    readonly server: Express,
+    useCase: FixGuest,
+    ...middleware: any[]
+  ) {
+    server.patch("/fix-guests/:id", ...middleware, async (req, res) => {
+      try {
+        const { id } = req.params
+        const {
+          name,
+          entryDate,
+          createdBy,
+          plate,
+          model,
+          pax,
+          apartment,
+          observation,
+          departureDate,
+          status,
+        } = req.body
+        await useCase.execute({
+          id,
+          name,
+          entryDate,
+          createdBy,
+          plate,
+          model,
+          pax,
+          apartment,
+          observation,
+          departureDate,
+          status,
+        })
+      } catch (error) {
+        if (error instanceof Error) {
+          return res.status(500).send(error.message).end()
+        }
+      }
+    })
+  }
+}
