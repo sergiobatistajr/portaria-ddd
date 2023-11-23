0e2f9b3184591705c8b4051cdee029b07b4b26a3
surfacez <sergiobatistajr94@gmail.com>
2023-11-23 10:54:01 -0300

    Refactor AllGuestDataTable map function

--- frontend/src/app/dashboard/report/AllGuestDataTable.tsx
+++ frontend/src/app/dashboard/report/AllGuestDataTable.tsx
@@ -11,22 +11,33 @@ export default async function AllGuestDataTable({
   currentPage: number
 }) {
   const data = await fetchAllGuestFiltered(query, currentPage)
-  const guests = data?.map((g) => {
-    return {
-      id: g.id,
-      nomeCompleto: g.name,
-      dataEntrada: formatDateToLocal(g.entryDate.toISOString()),
-      dataSaida: g.departureDate
-        ? formatDateToLocal(g.departureDate.toISOString())
-        : "-",
-      placa: g.plate ?? "-",
-      modelo: g.model ?? "-",
-      apartamento: g.apartment?.toString() ?? "-",
-      observacao: g.observation ?? "-",
-      passageiros: g.pax?.toString() ?? "-",
-      entradaPor: g.created_by_name,
+  const guests = data?.map(
+    (g: {
+      id: string
+      name: string
+      entryDate: string
+      departureDate: string
+      plate: string
+      model: string
+      apartment: number
+      observation: string
+      pax: number
+      created_by_name: string
+    }) => {
+      return {
+        id: g.id,
+        nomeCompleto: g.name,
+        dataEntrada: formatDateToLocal(g.entryDate),
+        dataSaida: g.departureDate ? formatDateToLocal(g.departureDate) : "-",
+        placa: g.plate ?? "-",
+        modelo: g.model ?? "-",
+        apartamento: g.apartment?.toString() ?? "-",
+        observacao: g.observation ?? "-",
+        passageiros: g.pax?.toString() ?? "-",
+        entradaPor: g.created_by_name,
+      }
     }
-  })
+  )
 
   return <DataTable columns={columns} data={guests ?? []} />
 }
