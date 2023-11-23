282c1e196c4a7ba372238bfabc14c9fceb1a0066
surfacez <sergiobatistajr94@gmail.com>
2023-11-23 16:05:29 -0300

    Refactor dashboard page layout

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,15 +1,22 @@
-import { auth } from "@/auth"
+import { Suspense } from "react"
+import GenerateChart from "./GenerateChart"
+import GuestChartSkeleton from "@/components/GuestChartSkeleton"
+import { Card } from "@/components/ui/card"
 
 export default async function Page() {
-  const session = await auth()
-
   return (
-    <div className="min-h-screen">
-      Home page
-      {session?.user?.email}
-      {session?.user?.name}
-      {session?.user?.role}
-      {session?.user?.status}
-    </div>
+    <main className="flex">
+      <div className="flex-1">
+        <Suspense fallback={<GuestChartSkeleton />}>
+          <GenerateChart />
+        </Suspense>
+      </div>
+      <div className="flex-shrink w-1/8">
+        <Card className="text-center">
+          <h1>Joao Sérgio</h1>
+          <p>300</p>
+        </Card>
+      </div>
+    </main>
   )
 }
