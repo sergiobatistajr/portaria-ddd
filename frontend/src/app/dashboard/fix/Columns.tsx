613ce8812486e83669d03cbc5465909a668d5706
surfacez <sergiobatistajr94@gmail.com>
2023-11-23 18:18:33 -0300

    Add Columns component for dashboard

---
+++ frontend/src/app/dashboard/fix/Columns.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import DialogExitGuest from "@/components/DialogExit"
+import { ColumnDef } from "@tanstack/react-table"
+type DTO = {
+  id: string
+  nomeCompleto: string
+  dataEntrada: string
+  dataSaida: string
+  placa: string
+  modelo: string
+  apartamento: string
+  observacao: string
+  entradaPor: string
+  passageiros: string
+}
+
+export const columns: ColumnDef<DTO>[] = [
+  {
+    accessorKey: "nomeCompleto",
+    header: "Nome completo",
+  },
+  {
+    accessorKey: "dataEntrada",
+    header: "Data de entrada",
+  },
+  {
+    accessorKey: "dataSaida",
+    header: "Data de saída",
+  },
+  {
+    accessorKey: "placa",
+    header: "Placa?",
+  },
+  {
+    accessorKey: "modelo",
+    header: "Modelo?",
+  },
+  {
+    accessorKey: "apartamento",
+    header: "Apartamento",
+  },
+  {
+    accessorKey: "observacao",
+    header: "Observação",
+  },
+  {
+    accessorKey: "passageiros",
+    header: "Passgeiros",
+  },
+  {
+    id: "actions",
+    header: "Corrigir",
+    cell: ({ row }) => {
+      const { id, nomeCompleto, placa, dataEntrada } = row.original
+      return <>...</>
+    },
+  },
+]
