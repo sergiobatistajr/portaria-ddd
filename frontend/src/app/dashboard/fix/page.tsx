35d74f30bdfac94966bb620a9eeceeb75756af6e
surfacez <sergiobatistajr94@gmail.com>
2023-11-23 18:18:26 -0300

    Add fix page component

---
+++ frontend/src/app/dashboard/fix/page.tsx
@@ -0,0 +1,32 @@
+import { fetchCountAllGuestsFilteredPages } from "@/lib/data"
+import Pagination from "@/components/Pagination"
+import { Suspense } from "react"
+import Search from "@/components/Search"
+import SkeletonDataTable from "@/components/SkeletonDataTable"
+import FixDataTable from "./FixDataTable"
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string
+    page?: string
+  }
+}) {
+  const query = searchParams?.query || ""
+  const currentPage = Number(searchParams?.page) || 1
+  const totalPages = (await fetchCountAllGuestsFilteredPages(query)) ?? 1
+
+  return (
+    <main>
+      <h1 className="flex text-3xl">Todos registros para correção</h1>
+      <div className="mt-2 space-y-2">
+        <Search placeholder="Procurar visitante..." />
+        <Suspense key={query + currentPage} fallback={<SkeletonDataTable />}>
+          <FixDataTable query={query} currentPage={currentPage} />
+        </Suspense>
+        <Pagination totalPages={parseInt(totalPages)} />
+      </div>
+    </main>
+  )
+}
