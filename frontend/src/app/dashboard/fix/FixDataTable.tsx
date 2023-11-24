7a9c31d0ce4a13cbf779f8694b883aa6909f2f30
surfacez <sergiobatistajr94@gmail.com>
2023-11-24 17:51:43 -0300

    Update formatDateToLocal to formatDateToISO in FixDataTable

--- frontend/src/app/dashboard/fix/FixDataTable.tsx
+++ frontend/src/app/dashboard/fix/FixDataTable.tsx
@@ -1,7 +1,7 @@
 import { fetchAllGuestFiltered } from "@/lib/data"
 import { columns } from "./Columns"
 import { DataTable } from "@/components/DataTable"
-import { formatDateToLocal } from "@/lib/utils"
+import { formatDateToISO } from "@/lib/utils"
 
 export default async function FixDataTable({
   query,
@@ -15,25 +15,31 @@ export default async function FixDataTable({
     (g: {
       id: string
       name: string
+      createdBy: string
       entryDate: string
-      departureDate: string
-      plate: string
-      model: string
-      apartment: number
-      observation: string
-      pax: number
+      status: string
+      departureDate?: string
+      plate?: string
+      model?: string
+      apartment?: number
+      observation?: string
+      pax?: number
       created_by_name: string
     }) => {
       return {
         id: g.id,
         nomeCompleto: g.name,
-        dataEntrada: formatDateToLocal(g.entryDate),
-        dataSaida: g.departureDate ? formatDateToLocal(g.departureDate) : "-",
-        placa: g.plate ?? "-",
-        modelo: g.model ?? "-",
-        apartamento: g.apartment?.toString() ?? "-",
-        observacao: g.observation ?? "-",
-        passageiros: g.pax?.toString() ?? "-",
+        dataEntrada: formatDateToISO(g.entryDate),
+        dataSaida: g.departureDate
+          ? formatDateToISO(g.departureDate)
+          : undefined,
+        placa: g.plate,
+        modelo: g.model,
+        apartamento: g.apartment?.toString(),
+        observacao: g.observation,
+        passageiros: g.pax?.toString(),
+        status: g.status,
+        entradaPor: g.createdBy,
       }
     }
   )
