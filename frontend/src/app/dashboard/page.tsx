377039597d1e2d54195c692284cabea732d7bb1c
surfacez <sergiobatistajr94@gmail.com>
2023-11-24 11:53:22 -0300

    Update ListUserCard data source

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,13 +2,10 @@ import { Suspense } from "react"
 import GenerateChart from "./GenerateChart"
 import GuestChartSkeleton from "@/components/GuestChartSkeleton"
 import ListUserCard from "@/components/ListUserCard"
-const data = [
-  {
-    name: "Sérgio Batista",
-    total: "12312312",
-  },
-]
+import { fetchListUserCard } from "@/lib/data"
+
 export default async function Page() {
+  const users = await fetchListUserCard()
   return (
     <main className="flex">
       <div className="flex-1">
@@ -17,7 +14,7 @@ export default async function Page() {
         </Suspense>
       </div>
       <div className="flex-shrink w-1/7 max-h-[650px] overflow-auto">
-        <ListUserCard users={data} />
+        <ListUserCard users={users} />
       </div>
     </main>
   )
